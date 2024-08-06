cdd5929138aae918485bf32ff2f4b95700b4f2fb
minhaj <minhaj101011@gmail.com>
2024-08-06 18:12:55 +0600

    hello

---
+++ secret.js
@@ -0,0 +1,6 @@
+const API_KEY = 'AIzaSyBWkvsUZPm-t1KM8xWgrVyhwK09pS6jPpc';
+const academicSheetID = '1UH9j_332yT0M4BtmySqQuvJmqfJIf2b54iwuShL7dtk'
+const oneshotSheetID = '1LOdOI0Q9d76Dz5uTTmhWq6ZSzD9GCwBFPfBS3FDWEgw';
+const examSheetID = '1uowLk3nSTefCcwnq6fx4GDcM481mgWJJ6sWqeeS2los';
+const apiKey = 'AIzaSyBooe-Fdm9nJRv1buEzPA8ayyRPPXtPQbs';
+const spreadsheetId = '1Sn0_s0_3KvlrrK90krhV9tUeax8yde-RVLNF10tHLqY';
